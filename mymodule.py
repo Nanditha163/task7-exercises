2a8bf99168125cd823552c981b9cbeb48f2e2306
Nanditha. U <74827999+Nanditha163@users.noreply.github.com>
2020-11-29 20:28:55 +0530

    Add files via upload

---
+++ mymodule.py
@@ -0,0 +1,7 @@
+def mylist():
+    listn=[10,11,12,13,14,15]
+    print(listn)
+    print("List after changing values:")
+    listn[1]=8
+    listn[2]=24
+    print(listn)
---
+++ task7ex3.py
@@ -0,0 +1,4 @@
+import random
+print("Printing random number from 1 to 100:")
+num1=random.randint(10,100)
+print("Random integer:",num1)
